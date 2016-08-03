b5f3ea57159e9b9755ec34cc83ae3e42ede232a6
Felipe Rosa <fksffr@gmail.com>
2016-08-03 19:15:36 -0300

    Create angular app using Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zazcarPrototypeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zazcarPrototypeApp
+ */
+angular.module('zazcarPrototypeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
